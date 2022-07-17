77a093863f0f815b8e387d20fb7b6177bd1aea46
RYuriy <radov.yuriy@ukr.net>
2022-07-17 17:42:21 +0300

    + setup Startup

--- Startup.cs
+++ Startup.cs
@@ -7,19 +7,34 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+ 
 
 namespace WebApp
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(opts => {
+                opts.UseSqlServer(Configuration["ConnectionStrings:ProductConnection"]);
+                opts.EnableSensitiveDataLogging(true);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DataContext context)
         {
             if (env.IsDevelopment())
             {
@@ -35,6 +50,7 @@ namespace WebApp
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+            SeedData.SeedDatabase(context);
         }
     }
 }
