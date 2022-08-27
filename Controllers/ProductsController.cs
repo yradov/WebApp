07173294261a61d89ddee340ac153b5073ac7bb4
RYuriy <radov.yuriy@ukr.net>
2022-08-27 22:37:30 +0300

    + Products Controller

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return new Product[]
+            {
+                new Product() {Name = "Product #1"},
+                new Product() {Name = "Product #2"},
+            };
+        }
+
+        [HttpGet("{id}")]
+        public Product GetProduct()
+        {
+            return new Product(){ProductId = 1, Name = "Test Product"};
+        }
+    }
+}
