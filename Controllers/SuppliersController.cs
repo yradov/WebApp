d7c472f08edb970ef66f8992418642ee62de11c3
RYuriy <radov.yuriy@ukr.net>
2022-10-29 20:00:55 +0300

    + SuppliersController

---
+++ Controllers/SuppliersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controler]")]
+    public class SuppliersController : Controller
+    {
+        private DataContext context;
+
+        public SuppliersController(DataContext ctx)
+        {
+            context = ctx;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Supplier> GetSupplier(long id)
+        {
+            return await context.Suppliers.FindAsync(id);
+        }
+    }
+}
